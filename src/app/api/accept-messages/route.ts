37ba976c624b4d4566537cbc54e3057838365026
usamaking786 <usamaahmadansari@gmail.com>
2025-06-02 13:05:46 +0500

    AI Integration for Messages

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,96 @@
+import { getServerSession } from "next-auth";
+import {authOptions} from "../auth/[...nextauth]/options"
+import dbConnect from "@/lib/dbConnect"
+import UserModel from "@/model/User";
+import {User} from "next-auth"
+
+export async function POST(request : Request){
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user : User = session?.user;
+    if(!session || !session.user){
+        return Response.json(
+            {success:false, message: "Not Authenticated"},{status:401}
+        );
+    }
+
+    const userId = user._id;
+    const {acceptMessages} = await request.json();
+    
+    try {
+       const updatedUser = await UserModel.findByIdAndUpdate(
+        userId,
+        {isAcceptingMessages : acceptMessages},
+        {new : true}
+       );
+
+       if(!updatedUser){
+        return Response.json({
+            success:false,
+            message: "Unable to find user to update message acceptance status",
+        },
+        {status:404}
+    );
+       }
+
+       return Response.json({
+        success:true,
+        message: "Message Acceptance Status updated successfully",
+        updatedUser,
+       },
+    {  status:200}
+    );
+
+    } catch (error) {
+    console.error("Error updating message acceptance status",error);
+    return Response.json({
+        success:false, message:"Error updating message acceptance status"},
+        {status: 500}
+    );
+    }
+
+}
+
+
+export async function GET(request: Request){
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+    if(!session || !session.user){
+        return Response.json({
+            success:false,message:"Not Authenticated"
+        },{
+            status:401
+        })
+    }
+
+    const userId = user._id;
+    const {acceptMessages} = await request.json();
+
+    try {
+      const foundUser = await UserModel.findById(user._id);
+      
+      if(!foundUser){
+        return Response.json(
+            {success:false, message:"User not found"},
+            {status:404}
+        )
+      }
+
+      return Response.json(
+        {success:true, isAcceptingMessages:foundUser.isAcceptingMessages},
+        {status:200}        
+      )
+      
+    } catch (error) {
+    
+    console.error("Error updating message acceptance status",error);
+    return Response.json({
+        success:false, message:"Error updating message acceptance status"},
+        {status: 500}
+    );
+
+    }
+}
+
