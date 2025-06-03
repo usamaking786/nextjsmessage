299ddbbbe5e0dc41d2b3a297ea73a9a5b51661e7
usamaking786 <usamaahmadansari@gmail.com>
2025-06-03 14:48:05 +0500

    Signup & Sign In Front End

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import {toast} from "sonner";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import {useRouter, useParams} from "next/navigation";
+import {useForm} from "react-hook-form";
+import * as z from "zod";
+import {verifySchema} from "@/schemas/verifySchema";
+import React from 'react'
+import { Form, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function verifyAccount() {
+  
+    const router = useRouter();
+    const params = useParams<{username: string}>();
+    const form = useForm<z.infer<typeof verifySchema>>({
+        resolver:zodResolver(verifySchema)
+    });
+
+
+    const onSubmit = async(data: z.infer<typeof verifySchema>)=>{
+        try {
+            const response = await axios.post<ApiResponse>(`/api/verify-code`, {
+                username: params.username,
+                code : data.verifyCode,
+            });
+            toast.success(response.data.message);
+            router.replace("/sign-in");
+
+
+        } catch (error) {
+            const axiosError = error as AxiosError<ApiResponse>;
+            toast.message("Verification failed",{
+                description:axiosError.response?.data.message ?? 'An error occurred. Please try again.'
+            })
+        }
+    }
+    return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+            <div className="text-center">
+                <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+
+            </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="verifyCode"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+
+        </div>
+    </div>
+  )
+}
